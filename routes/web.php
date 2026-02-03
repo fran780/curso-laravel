961fe599df9b85a10764913e4a1f7e2fe0a87cc2
Fran Fernandez <franciscofernandez365@yahoo.com>
2026-02-03 16:46:50 -0600

    lección 4

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,33 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function () {
+    return 'Pagina de inicio';
+});
+
+Route::get('/notas', function () {
+    return 'Listado de notas';
+});
+
+Route::get('/notas/{id}', function ($id) {
+    return 'Detalles de la nota: ' .$id;
+})->whereNumber('id');
+
+Route::get('notas/crear', function () {
+    return 'Crear nueva nota';
+}); 
+
+Route::get('/notas/{id}/editar', function ($id) {
+    return 'Editar nota: ' .$id;
+});
+
+Route::get('cursos', function () {
+    return [
+    'Cursos' =>
+        [
+            'Curso de Laravel 10',
+            'Curso de programacion orientada a objetos',
+            'Curso de Git y Github'
+        ]
+    ];
 });
