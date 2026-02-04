3b6abce5f8fda762c0a63bd32ec6b4df9d7405f3
Fran Fernandez <franciscofernandez365@yahoo.com>
2026-02-04 13:28:50 -0600

    Layouts tradicionales con Blade

--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -1,33 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-    <link rel="stylesheet" type="text/css" href= "{{ asset('css/app.css')  }}"> <!-- Ruta relativa al archivo CSS en caso de que falle, por eso se ponen los ../ en caso de que se agregue un tercer archivo a la ruta pues entonces solo deja con / o sea a partir del dominio -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">     <!-- Ademas ahora se le puso un asset que lo hace es completar la ruta correcta por mi -->
-</head>
-<body>
-    <div class="wrap">
-        <header class="head">
-            <a href="#" class="logo"></a>
+@extends('layouts.app') <!-- con el extends se hereda una plantilla principal que en este caso es la app.blade.php -->
 
-            <nav class="main-nav">
-                <ul class="main-nav-list">
-                    <li class="main-nav-item">
-                        <a href="/static/notes.html" class="main-nav-link">
-                            <i class="icon icon-th-list"></i>
-                            <span>Ver notas</span>
-                        </a>
-                    </li>
-                    <li class="main-nav-item active">
-                        <a href="/static/add-note.html" class="main-nav-link">
-                            <i class="icon icon-pen"></i>
-                            <span>Nueva nota</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+@section('title', 'Crear Nota')
+
+@section('content') <!-- con el section se define una seccion que en este caso es la seccion content que esta en la plantilla principal app.blade.php -->
+
         <main class="content">
             <div class="cards">
                 <div class="card card-center">
@@ -47,17 +23,5 @@
                 </div>
             </div>
         </main>
-        <footer class="foot">
-            <div class="ad">
-                <p>
-                    Esta aplicación es desarrollada en el curso
-                    <a href="https://styde.net/laravel-6">Primeros pasos con Laravel 6</a>.
-                </p>
-            </div>
-            <div class="license">
-                <p>© 2019 Derechos Reservados - Styde Limited</p>
-            </div>
-        </footer>
-    </div>
-</body>
-</html>
+
+@endsection
