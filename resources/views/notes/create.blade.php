a30076f9e3d79edecaf4f5ab734c518c0aec3284
Fran Fernandez <franciscofernandez365@yahoo.com>
2026-02-06 13:04:01 -0600

    Validación de datos con Laravel 10

--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -12,11 +12,22 @@
                     <form action=" {{ route('notes.store') }}" method="POST">
                         <!-- route('notes.store') es una funcion de laravel que genera la url de la ruta notes.store definida en web.php -->
                         @csrf
+
                         <label for="title" class="field-label">Título: </label>
-                        <input type="text" name="title" id="title" class="field-input">
+                        <input type="text" name="title" id="title" value="{{ old('title') }}"
+                            class="field-input @error('title') field-error @enderror">
+                        <!-- aqui se agrega una clase condicional field-error si hay un error de validacion en el campo title -->
+
+                        <!-- este error sirve para validaciones donde se muestra un mensaje al tener el campo vacio -->
+                        @error('title')
+                            <p class="error-message">{{ $message }}</p>
+                        @enderror
 
                         <label for="content" class="field-label">Contenido:</label>
-                        <textarea name="content" id="content" rows="10" class="field-textarea"></textarea>
+                        <textarea name="content" id="content" rows="10" class="field-textarea @error('content') field-error @enderror">{{ old('content') }}</textarea>
+                        @error('content')
+                            <p class="error-message">{{ $message }}</p>
+                        @enderror
 
                         <button type="submit" class="btn btn-primary">Crear nota</button>
                     </form>
